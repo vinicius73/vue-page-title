1a7739b73a10d480f40af3b8bb4977b8368fca7e
Vinicius Reis <luiz.vinicius73@gmail.com>
2018-06-23 18:48:01 -0300

    add banner and es format

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,19 @@
-const formats = ['iife', 'cjs', 'umd', 'amd']
+const pkg = require('./package.json')
 
+const formats = ['iife', 'es', 'cjs', 'umd', 'amd']
+const year = new Date().getFullYear()
+const yearString = (year === 2018) ? '2018' : `2018-${year}`
+
+const banner = `// ${pkg.name} v${pkg.version}
+// ${pkg.description}
+// ${pkg.repository}
+// (c) ${yearString} ${pkg.author}
+`
 export default {
   input: 'src/index.js',
   output: formats.map(format => ({
     format,
+    banner,
     exports: 'named',
     name: 'VuePageTitle',
     file: `index.${format}.js`,
