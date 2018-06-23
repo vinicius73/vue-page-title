b7234927d9e4337d95047c9e4430d3f20a2625d2
Vinicius Reis <luiz.vinicius73@gmail.com>
2018-06-23 17:48:24 -0300

    make reactive title

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-export const x = {}
+import { isFunction } from './utils'
+
+const install = (Vue, options = {}) => {
+  // prevent double install
+  if (install.installed) return
+  install.installed = true
+
+  // title state
+  const $page = {
+    title: ''
+  }
+
+  // make reactive title
+  Vue.util.defineReactive($page, 'title', '')
+
+  // add title to component context
+  Object.defineProperty(Vue.prototype, '$title', {
+    get: () => $page.title,
+    set: value => {
+      $page.title = value
+    }
+  })
+
+  // add global mixin
+  Vue.mixin({
+    created () {
+      const { title } = this.$options
+
+      if (title !== undefined) {
+        // allow use dinamic title system
+        this.$title = isFunction(title)
+          ? title(this)
+          : title
+      }
+    }
+  })
+}
+
+export { install }
---
+++ src/utils.js
@@ -0,0 +1,30 @@
+/**
+ * test if object is a function
+ * @method isFunction
+ * @param  {any}      object
+ * @return {Boolean}
+ */
+const isFunction = object => typeof (object) === 'function'
+
+/**
+ * prevent output undefined
+ * @method safeString
+ * @param  {String}   st
+ * @return {String}
+ */
+const safeString = st => st || ''
+
+/**
+ * build a full title white suffix and prefix
+ * @method buildPageTitle
+ * @param  {String}       value
+ * @param  {Object}       [options={ suffix, prefix }]
+ * @return {String}       full title
+ */
+const buildPageTitle = (value, options = { }) => {
+  const { prefix, suffix } = options
+
+  return `${safeString(prefix)} ${value} ${safeString(suffix)}`.trim()
+}
+
+export { buildPageTitle, isFunction }
