fe902116375ec173510ab4b4feae04a7b54bc4ef
Vinicius Reis <luiz.vinicius73@gmail.com>
2018-06-23 18:42:29 -0300

    output more formats

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,12 @@
+const formats = ['iife', 'cjs', 'umd', 'amd']
+
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'index.cjs.js',
-    dir: 'dist',
-    format: 'cjs'
-  }
+  output: formats.map(format => ({
+    format,
+    exports: 'named',
+    name: 'VuePageTitle',
+    file: `index.${format}.js`,
+    dir: 'dist'
+  }))
 }
