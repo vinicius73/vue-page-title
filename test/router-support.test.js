2f8e07ef7f86af5f59b030ecd6f16d910df9874c
Vinicius Reis <luiz.vinicius73@gmail.com>
2018-06-27 01:03:14 -0300

    router tests

---
+++ test/router-support.test.js
@@ -0,0 +1,25 @@
+import VuePageTitle from '../src/index'
+
+import { createLocalVue } from '@vue/test-utils'
+import test from 'ava'
+
+test('setup', t => {
+  t.plan(1)
+  const localVue = createLocalVue()
+  const router = {
+    afterEach: fn => {
+      const to = {
+        meta: {
+          title: 'Graal'
+        }
+      }
+
+      fn(to)
+      fn({}) // for covarage purposes
+
+      t.is(localVue.prototype.$title, 'Graal')
+    }
+  }
+
+  localVue.use(VuePageTitle, { router })
+})
