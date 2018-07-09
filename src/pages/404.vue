596cd77818e485c0bc281ddbc1569a4d8160cfcb
Vinicius Reis <luiz.vinicius73@gmail.com>
2018-07-09 15:19:50 -0300

    page 404

---
+++ src/pages/404.vue
@@ -0,0 +1,39 @@
+<script>
+const titles = [
+  'Something bad is happening...',
+  'I have bad feeling about this',
+  'Have you tried a different address?',
+  'You should not be here...',
+  'Maybe it was a hacker... or a cracker...',
+  'Why do not you take a nap and take a nap?',
+  'I swear I do not know how this can happen...',
+  'Do you trust the person who passed this link?',
+  'There is nothing for you to see here ... keep walking.'
+]
+
+const getTitle = () => titles[Math.floor(Math.random() * titles.length)]
+
+export default {
+  name: 'page-404',
+  title: () => getTitle(),
+  mounted () {
+    this.$interval = setInterval(() => {
+      this.$title = getTitle()
+    }, 15000)
+  },
+  beforeDestroy () {
+    clearInterval(this.$interval)
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="title is-1">404 - {{ $title }}</h1>
+    <div v-if="$apollo.loading" class="has-text-centered">
+      <span class="icon">
+        <i class="fas fa-spinner fa-pulse fa-3x"></i>
+      </span>
+    </div>
+  </div>
+</template>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,6 +1,7 @@
 const Home = () => import(/* webpackChunkName: "page-home" */ '../pages/home.vue')
 const About = () => import(/* webpackChunkName: "page-about" */ '../pages/about/index.vue')
-const Project = () => import(/* webpackChunkName: "page-about" */ '../pages/project/index.vue')
+const Project = () => import(/* webpackChunkName: "page-project" */ '../pages/project/index.vue')
+const Page404 = () => import(/* webpackChunkName: "page-404" */ '../pages/404.vue')
 
 const routes = [{
   path: '/',
@@ -14,6 +15,9 @@ const routes = [{
   path: '/project/:id',
   name: 'project',
   component: Project
+}, {
+  path: '*',
+  component: Page404
 }]
 
 export default routes
