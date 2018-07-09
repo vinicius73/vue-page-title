0c253655a9bdfba592c7f587cc22c09657afa6a4
Vinicius Reis <luiz.vinicius73@gmail.com>
2018-07-09 15:44:44 -0300

    fix and add more quotes

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -5,10 +5,11 @@ const titles = [
   'Have you tried a different address?',
   'You should not be here...',
   'Maybe it was a hacker... or a cracker...',
-  'Why do not you take a nap and take a nap?',
+  'Why do not you take that break and take a nap?',
   'I swear I do not know how this can happen...',
   'Do you trust the person who passed this link?',
-  'There is nothing for you to see here ... keep walking.'
+  'There is nothing for you to see here... Keep walking.',
+  'On my machine it works...'
 ]
 
 const getTitle = () => titles[Math.floor(Math.random() * titles.length)]
