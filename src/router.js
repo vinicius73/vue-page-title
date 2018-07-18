ec3bcb64f51a67557e5cd40e1a086423df07ed16
John Langone <johnlangone@gmail.com>
2018-07-18 14:45:46 -0400

    added titlePrefix and titleSuffix properties inline with title.

--- src/router.js
+++ src/router.js
@@ -4,13 +4,26 @@
  * @param  {Object}     options     { router: RouterInstance }
  * @return {void}
  */
-const setup = (setTitle, { router }) => {
+const setup = (setTitle, setPrefix, setSuffix, { router }) => {
   router.afterEach((to, from) => {
     const { meta } = to
 
-    // if has meta and title
-    if (meta && meta.title) {
-      setTitle(meta.title)
+    // if has meta
+    if (meta) {
+      // if has title
+      if (meta.title) {
+        setTitle(meta.title)
+      }
+
+      // if has titlePrefix
+      if (meta.titlePrefix) {
+        setPrefix(meta.titlePrefix)
+      }
+
+      // if has titleSuffix
+      if (meta.titleSuffix) {
+        setSuffix(meta.titleSuffix)
+      }
     }
   })
 }
