75c1425f80b90fd460f523c826cbcd7b96de2cec
Vinicius Reis <luiz.vinicius73@gmail.com>
2020-04-01 13:52:40 -0300

    ✨ add babel support

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,20 @@
+const babel = require('rollup-plugin-babel')
 const { terser } = require('rollup-plugin-terser')
 const pkg = require('./package.json')
+
+const useBabel = process.env.USE_BABEL === 'yes'
 
 const formats = ['iife', 'es', 'cjs', 'umd', 'amd']
 const year = new Date().getFullYear()
 const yearString = (year === 2018) ? '2018' : `2018-${year}`
+
+const plugins = [
+  useBabel && babel({
+    exclude: 'node_modules/**'
+  })
+].filter(Boolean)
+
+const outputPath = useBabel ? 'dist/es5' : 'dist'
 
 const banner = `/*! ${pkg.name} v${pkg.version}
  * ${pkg.description}
@@ -13,19 +24,20 @@ const banner = `/*! ${pkg.name} v${pkg.version}
 `
 export default {
   input: 'src/index.js',
+  plugins,
   output: formats.flatMap(format => {
     return [{
       format,
       banner,
       exports: 'named',
       name: 'VuePageTitle',
-      file: `dist/index.${format}.js`
+      file: `${outputPath}/index.${format}.js`
     }, {
       format,
       banner,
       exports: 'named',
       name: 'VuePageTitle',
-      file: `dist/index.${format}.min.js`,
+      file: `${outputPath}/index.${format}.min.js`,
       plugins: [terser()]
     }]
   })
