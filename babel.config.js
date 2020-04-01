fb79c5cf0a301b9557b13a4de2238aa228f9625a
Vinicius Reis <luiz.vinicius73@gmail.com>
2020-04-01 14:32:05 -0300

    ✅ fix missing babel test config

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,13 @@
+const isTest = process.env.NODE_ENV === 'test'
+
 module.exports = {
   presets: [
     [
-      '@babel/preset-env'
+      '@babel/preset-env',
+      isTest ? {
+        corejs: '2',
+        useBuiltIns: 'usage'
+      } : {}
     ]
   ]
 }
