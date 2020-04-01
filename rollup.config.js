fcfa20bf51394302131d8c174a52ea8a46d447c1
Vinicius Reis <luiz.vinicius73@gmail.com>
2020-04-01 13:38:52 -0300

    ✨ generate minified output

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,32 @@
+const { terser } = require('rollup-plugin-terser')
 const pkg = require('./package.json')
 
 const formats = ['iife', 'es', 'cjs', 'umd', 'amd']
 const year = new Date().getFullYear()
 const yearString = (year === 2018) ? '2018' : `2018-${year}`
 
-const banner = `// ${pkg.name} v${pkg.version}
-// ${pkg.description}
-// ${pkg.repository}
-// (c) ${yearString} ${pkg.author}
+const banner = `/*! ${pkg.name} v${pkg.version}
+ * ${pkg.description}
+ * ${pkg.repository}
+ * (c) ${yearString} ${pkg.author}
+ */
 `
 export default {
   input: 'src/index.js',
-  output: formats.map(format => ({
-    format,
-    banner,
-    exports: 'named',
-    name: 'VuePageTitle',
-    file: `dist/index.${format}.js`
-  }))
+  output: formats.flatMap(format => {
+    return [{
+      format,
+      banner,
+      exports: 'named',
+      name: 'VuePageTitle',
+      file: `dist/index.${format}.js`
+    }, {
+      format,
+      banner,
+      exports: 'named',
+      name: 'VuePageTitle',
+      file: `dist/index.${format}.min.js`,
+      plugins: [terser()]
+    }]
+  })
 }
