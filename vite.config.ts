af8a819b1d029131e431642146b2d966afb7f2dc
Vinicius Reis <luiz.vinicius73@gmail.com>
2022-08-28 12:40:38 -0300

    📦️ (#19): adjust package

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,49 @@
 import { defineConfig } from 'vite'
+import banner from "vite-plugin-banner";
 import vue from "@vitejs/plugin-vue";
+import pkg from "./package.json";
+
+const year = new Date().getFullYear();
+const yearString = year === 2018 ? "2018" : `2018-${year}`;
+
+const minify = process.env.MINIFY === "yes";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    banner(`/*! ${pkg.name} v${pkg.version}
+ * ${pkg.description}
+ * ${pkg.repository}
+ * (c) ${yearString} ${pkg.author}
+ */
+`),
+  ],
   build: {
-    minify: false,
+    ...(minify
+      ? {
+          emptyOutDir: false,
+        }
+      : {
+          minify: false,
+        }),
     lib: {
       entry: "lib/index.ts",
       name: "VuePageTitle",
-      formats: ["cjs", "es"],
+      fileName: (format) => {
+        if (minify) {
+          return `index.${format}.min.js`;
+        }
+        return `index.${format}.js`;
+      },
+      formats: ["cjs", "es", "iife", "umd"],
     },
     rollupOptions: {
-      external: ['vue']
-    }
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
   },
 });
